a75718b9d836353060d1fc8bb9bce9abd42d0e34
BorisPanich <panicg2303@gmail.com>
2021-03-25 14:18:56 +0300

    24

---
+++ Mumbling.js
@@ -0,0 +1,10 @@
+function accum(s) {
+	const letters = s.toLowerCase().split('');
+  let solution = [];
+  for (let i = 0; i < letters.length; i++) {
+    solution.push(letters[i].repeat(i + 1))
+
+  }
+  solution = solution.map(str => str.replace(/\b\w/g, letters => letters.toUpperCase())).join('-');
+  return solution;
+}
