eca1b001047dce194b0c082f1d8a14a559dc2f93
BorisPanich <panich2303@gmail.com>
2022-04-14 06:42:08 +0300

    Sum of positive

---
+++ Python/Sum of positive.py
@@ -0,0 +1,14 @@
+def positive_sum(arr):
+    sum = 0
+    for e in arr:
+        if e > 0:
+            sum += e
+    return sum
+
+
+def positive_sum(arr):
+    return sum(x for x in arr if x > 0)
+# return sum([x for x in arr if x==abs(x)])
+
+
+positive_sum = lambda a: sum(e for e in a if e > 0)
