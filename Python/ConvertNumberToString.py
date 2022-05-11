f9200da974a6bc7d18025629374ca1bc55f29412
BorisPanich <panich2303@gmail.com>
2022-05-11 06:11:46 +0300

    Convert a Number to a String!

---
+++ Python/ConvertNumberToString.py
@@ -0,0 +1,22 @@
+def number_to_string(num):
+    return str(num)
+
+
+def number_to_string(num):
+    try:
+        return str(num)
+    except:
+        return None
+
+
+number_to_string = lambda n: str(n)
+
+number_to_string = str
+
+
+def number_to_string(num):
+    return f"{num}"
+
+
+def number_to_string(num):
+    return num.__str__()
