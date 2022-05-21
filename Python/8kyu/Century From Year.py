90a60882be686293e7fa7b7976b849f3d40f44a5
BorisPanich <panich2303@gmail.com>
2022-05-21 06:21:25 +0300

    Century From Year

---
+++ Python/8kyu/Century From Year.py
@@ -0,0 +1,13 @@
+import math
+
+
+def century(year):
+    return math.ceil(year / 100)
+
+
+def century(year):
+    return __import__('math').ceil(year / 100)
+
+
+century = lambda year: math.ceil(year / 100)
+century = lambda _: math.ceil(_ / 100)
